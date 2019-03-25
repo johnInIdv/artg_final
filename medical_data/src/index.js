4e65f2d20a752d152b0ea2213e769167ffb110a1
John Lilly <lilly.j@husky.neu.edu>
2019-03-24 22:25:59 -0400

    all works, but need to update the data in adbomenViz to make it dynamic.

--- medical_data/src/index.js
+++ medical_data/src/index.js
@@ -3,7 +3,7 @@ import './bootstrap_bertin/dist/css/bootstrap.css';
 import * as d3 from 'd3';
 
 import {
-	// abdomenViz,
+	abdomenViz,
 	// abdomenForm,
 	// kneeViz,
 	// abdomenElements,
@@ -214,9 +214,9 @@ p.addEventListener('change',(e) => {
 	// build scene and set duration to window height
   var scene1 = new ScrollMagic.Scene({triggerElement: "#firstTrigger",duration:200,offset: 400})
           .setTween(tween1)
-          // .on('enter',() => {
-					// 	globalDispatch.call('make:bars',null);
-          // })
+          .on('enter',() => {
+						globalDispatch.call('make:bars',null);
+          })
           .addIndicators({name:"trigger #1"}) // add indicators (requires plugin)
           .addTo(controller);
 
--- medical_data/src/utils.js
+++ medical_data/src/utils.js
@@ -1,13 +1,13 @@
 import * as d3 from 'd3';
 
 import {
-	theData,
-} from './data';
+	theData2,
+} from './data2';
 
 
 
 // labels = labels (all the variables for the chosen problem)
-// inputID = id's for the chosen variable so that it's value can be matched against values in theData set.
+// inputID = id's for the chosen variable so that it's value can be matched against values in theData2 set.
 // problemsElements = the variables for a particular problem's element
 // const displayElements = function(labels, inputID, problemsElements,labelsChecked,inputIDchecked,checked){
 //
@@ -146,6 +146,10 @@ import {
 // send the array of labels and inputsID's to the globalDispatch
 
 const globalDispatch = d3.dispatch('make:bars','store:labels','update:first');
+
+globalDispatch.on('make:bars', () => {
+	abdomenViz();
+});
 
 function FormDisplay(){
 
@@ -316,36 +320,36 @@ function abdomenViz(){
    }}
 
   var i;
-  for (i = 0; i < 3; i++){//remember to change the length number when data objects are added to the data
-    if ((theNewData.symptoms.age) == (theData.abdomen[i].symptoms.age)&&
-        (theNewData.symptoms.gender) == (theData.abdomen[i].symptoms.gender)&&
-        (theNewData.symptoms.location) == (theData.abdomen[i].symptoms.location)&&
-        (theNewData.symptoms.time) == (theData.abdomen[i].symptoms.time)&&
-        (theNewData.symptoms.pain) == (theData.abdomen[i].symptoms.pain)&&
-        (theNewData.symptoms.fever) == (theData.abdomen[i].symptoms.fever)&&
-        (theNewData.symptoms.vomiting) == (theData.abdomen[i].symptoms.vomiting)&&
-        (theNewData.symptoms.blood_in_vomit) == (theData.abdomen[i].symptoms.blood_in_vomit)&&
-        (theNewData.symptoms.diarrhea) == (theData.abdomen[i].symptoms.diarrhea)&&
-        (theNewData.symptoms.blood_in_stool) == (theData.abdomen[i].symptoms.blood_in_stool)&&
-        (theNewData.symptoms.risk_factors) == (theData.abdomen[i].symptoms.risk_factors)
+  // for (i = 0; i < 3; i++){//remember to change the length number when data objects are added to the data
+    if ((theNewData.symptoms.age) == (theData2.abdomen.instances.one.symptoms.age)&&
+        (theNewData.symptoms.gender) == (theData2.abdomen.instances.one.symptoms.gender)&&
+        (theNewData.symptoms.location) == (theData2.abdomen.instances.one.symptoms.location)&&
+        (theNewData.symptoms.time) == (theData2.abdomen.instances.one.symptoms.time)&&
+        (theNewData.symptoms.pain) == (theData2.abdomen.instances.one.symptoms.pain)&&
+        (theNewData.symptoms.fever) == (theData2.abdomen.instances.one.symptoms.fever)&&
+        (theNewData.symptoms.vomiting) == (theData2.abdomen.instances.one.symptoms.vomiting)&&
+        (theNewData.symptoms.blood_in_vomit) == (theData2.abdomen.instances.one.symptoms.blood_in_vomit)&&
+        (theNewData.symptoms.diarrhea) == (theData2.abdomen.instances.one.symptoms.diarrhea)&&
+        (theNewData.symptoms.blood_in_stool) == (theData2.abdomen.instances.one.symptoms.blood_in_stool)&&
+        (theNewData.symptoms.risk_factors) == (theData2.abdomen.instances.one.symptoms.risk_factors)
   ){
-    // return {theData[i]};
-    riskData = [theData.abdomen[i].actions.ER,
-                theData.abdomen[i].actions.urgent_care,
-                theData.abdomen[i].actions.primary_care,
-                theData.abdomen[i].actions.nothing];
-    // riskData.push(theData.abdomen[i].actions.risk);
+    // return {theData2[i]};
+    riskData = [theData2.abdomen.instances.one.actions.ER,
+                theData2.abdomen.instances.one.actions.urgent_care,
+                theData2.abdomen.instances.one.actions.primary_care,
+                theData2.abdomen.instances.one.actions.nothing];
+    // riskData.push(theData2.abdomen[i].actions.risk);
 
-    console.log("the risk while going to ER: " + theData.abdomen[i].actions.ER);
-    console.log("the risk while going to Urgent Care: " + theData.abdomen[i].actions.urgent_care);
-		console.log("the risk while going to primary doc: " + theData.abdomen[i].actions.primary_care);
-		console.log("the risk of doing nothing: " + theData.abdomen[i].actions.nothing);
+    console.log("the risk while going to ER: " + theData2.abdomen.instances.one.actions.ER);
+    console.log("the risk while going to Urgent Care: " + theData2.abdomen.instances.one.actions.urgent_care);
+		console.log("the risk while going to primary doc: " + theData2.abdomen.instances.one.actions.primary_care);
+		console.log("the risk of doing nothing: " + theData2.abdomen.instances.one.actions.nothing);
 
     }
    else {
      console.log("not the same");
    }
-}
+// }
 
 barVizFunction(riskData);
 }
@@ -367,27 +371,27 @@ barVizFunction(riskData);
 //
 //   var i;
 //   for (i = 0; i < 3; i++){//remember to change the length number when data objects are added to the data
-//     if ((theNewData.symptoms.age) == (theData.[i].symptoms.age)&&
-//         (theNewData.symptoms.gender) == (theData.[i].symptoms.gender)&&
-//         (theNewData.symptoms.location) == (theData.[i].symptoms.location)&&
-//         (theNewData.symptoms.time) == (theData.[i].symptoms.time)&&
-//         (theNewData.symptoms.pain) == (theData.[i].symptoms.pain)&&
-//         (theNewData.symptoms.fever) == (theData.[i].symptoms.fever)&&
-//         (theNewData.symptoms.vomiting) == (theData.[i].symptoms.vomiting)&&
-//         (theNewData.symptoms.blood_in_vomit) == (theData.[i].symptoms.blood_in_vomit)&&
-//         (theNewData.symptoms.risk_factors) == (theData.[i].symptoms.risk_factors)
+//     if ((theNewData.symptoms.age) == (theData2.[i].symptoms.age)&&
+//         (theNewData.symptoms.gender) == (theData2.[i].symptoms.gender)&&
+//         (theNewData.symptoms.location) == (theData2.[i].symptoms.location)&&
+//         (theNewData.symptoms.time) == (theData2.[i].symptoms.time)&&
+//         (theNewData.symptoms.pain) == (theData2.[i].symptoms.pain)&&
+//         (theNewData.symptoms.fever) == (theData2.[i].symptoms.fever)&&
+//         (theNewData.symptoms.vomiting) == (theData2.[i].symptoms.vomiting)&&
+//         (theNewData.symptoms.blood_in_vomit) == (theData2.[i].symptoms.blood_in_vomit)&&
+//         (theNewData.symptoms.risk_factors) == (theData2.[i].symptoms.risk_factors)
 //   ){
-//     // return {theData[i]};
-//     riskData = [theData.[i].actions.ER,
-//                 theData.[i].actions.urgent_care,
-//                 theData.[i].actions.primary_care,
-//                 theData.[i].actions.nothing];
-//     // riskData.push(theData.abdomen[i].actions.risk);
+//     // return {theData2[i]};
+//     riskData = [theData2.[i].actions.ER,
+//                 theData2.[i].actions.urgent_care,
+//                 theData2.[i].actions.primary_care,
+//                 theData2.[i].actions.nothing];
+//     // riskData.push(theData2.abdomen[i].actions.risk);
 //
-//     console.log("the risk while going to ER: " + theData.[i].actions.ER);
-//     console.log("the risk while going to Urgent Care: " + theData.[i].actions.urgent_care);
-// 		console.log("the risk while going to primary doc: " + theData.[i].actions.primary_care);
-// 		console.log("the risk of doing nothing: " + theData.[i].actions.nothing);
+//     console.log("the risk while going to ER: " + theData2.[i].actions.ER);
+//     console.log("the risk while going to Urgent Care: " + theData2.[i].actions.urgent_care);
+// 		console.log("the risk while going to primary doc: " + theData2.[i].actions.primary_care);
+// 		console.log("the risk of doing nothing: " + theData2.[i].actions.nothing);
 //
 //     }
 //    else {
@@ -397,57 +401,57 @@ barVizFunction(riskData);
 //
 // barVizFunction(riskData);
 // }
-//
-// // const w = d3.select('.plot').node().clientWidth;
-// // const h = d3.select('.plot').node().clientHeight;
-// const w = 600;
-// const h = 600;
-// const barViz = d3.select('#svg').append('svg').attr('width',w).attr('height',h);
-//
-// const barVizFunction = function(data){
-//
-//           //UPDATE SELECTION
-//         	const nodes = barViz.selectAll('.node')
-//         		.data(data);
-//         	nodes.select('rect')
-//         		//.transition()
-//         		.style('fill','green'); //circles in the UPDATE selection are black; note the use of .transition
-//
-//         	//ENTER SELECTION
-//         	const nodesEnter = nodes.enter()
-//         		.append('g').attr('class', 'node');
-//         	nodesEnter.append('rect')
-//         		// .style('fill', 'red'); //circles in the ENTER selection are colored differently
-//         	nodesEnter.append('text')
-//         		.attr('text-anchor', 'middle');
-//
-//         	//UPDATE + ENTER
-//         	//Both of their 'transform' attributes are the set the same way
-//         	//so we merge them
-//         	nodes.merge(nodesEnter)
-//         		.transition()
-//         		// .attr('transform', d => `translate(${d.x}, ${d.y})`);
-//         	nodes.merge(nodesEnter)
-//         		.select('text')
-//         		.text(d => d);
-//         	nodes.merge(nodesEnter)
-//         		.select('rect')
-//         		.transition()
-//             .attr("x", 150)
-//             .attr("y", function (d,i) { return i * 20 -2; })
-//             .attr("width", function (d){return d * 7})
-//             .attr("height", 10)
-//             .attr("fill", 'red');
-//
-//         	//EXIT SELECTION
-//         	//nodes.exit().remove();
-//         	nodes.exit()
-//         		.select('rect')
-//         		.style('fill','green');
-// }
+
+// const w = d3.select('.plot').node().clientWidth;
+// const h = d3.select('.plot').node().clientHeight;
+const w = 600;
+const h = 600;
+const barViz = d3.select('#svg').append('svg').attr('width',w).attr('height',h);
+
+const barVizFunction = function(data){
+
+          //UPDATE SELECTION
+        	const nodes = barViz.selectAll('.node')
+        		.data(data);
+        	nodes.select('rect')
+        		//.transition()
+        		.style('fill','green'); //circles in the UPDATE selection are black; note the use of .transition
+
+        	//ENTER SELECTION
+        	const nodesEnter = nodes.enter()
+        		.append('g').attr('class', 'node');
+        	nodesEnter.append('rect')
+        		// .style('fill', 'red'); //circles in the ENTER selection are colored differently
+        	nodesEnter.append('text')
+        		.attr('text-anchor', 'middle');
+
+        	//UPDATE + ENTER
+        	//Both of their 'transform' attributes are the set the same way
+        	//so we merge them
+        	nodes.merge(nodesEnter)
+        		.transition()
+        		// .attr('transform', d => `translate(${d.x}, ${d.y})`);
+        	nodes.merge(nodesEnter)
+        		.select('text')
+        		.text(d => d);
+        	nodes.merge(nodesEnter)
+        		.select('rect')
+        		.transition()
+            .attr("x", 150)
+            .attr("y", function (d,i) { return i * 20 -2; })
+            .attr("width", function (d){return d * 7})
+            .attr("height", 10)
+            .attr("fill", 'red');
+
+        	//EXIT SELECTION
+        	//nodes.exit().remove();
+        	nodes.exit()
+        		.select('rect')
+        		.style('fill','green');
+}
 
 export {
-  // abdomenViz,
+  abdomenViz,
   // kneeViz,
   // abdomenForm,
   // abdomenElements,
